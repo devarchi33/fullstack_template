6996a7b1bdaef147ba621cbf9b9b772bfebe172b
skyfly33 <imfly7@naver.com>
2015-07-30 02:53:23 +0900

    kaka story button 2 add.

--- client/app/kakao/kakao.controller.js
+++ client/app/kakao/kakao.controller.js
@@ -3,4 +3,17 @@
 angular.module('skyfly33App')
 .controller('KakaoCtrl', function ($scope) {
 	$scope.message = 'Hello';
+
+	// 스토리 공유 버튼을 생성합니다.
+	Kakao.Story.createShareButton({
+		container: '#kakaostory-share-button',
+		url: 'http://www.naver.com',
+		text: '카카오 개발자 사이트로 놀러오세요! #개발자 #카카오 :)'
+	});
+
+	// 스토리 구독 버튼을 생성합니다.
+	Kakao.Story.createFollowButton({
+		container: '#kakaostory-follow-button',
+		id: 'daumkakao'
+	});
 });
